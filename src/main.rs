d4e8ef4856a4869d2b2ae33a6758d6369896573a
davimouravilaca <davimouravilaca@gmail.com>
2025-04-13 16:39:15 -0300

    camera

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,29 @@
 mod map;
 mod map_builder;
 mod player;
+mod camera;
 
-// limit viewport
-pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
-pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
 
 mod prelude {
+
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
+    // limit viewport
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
     pub use crate::map::*;
     pub use crate::player::*;
     pub use crate::map_builder::*;
+    pub use crate::camera::*;
 }
 
 use prelude::*;
 
 struct State {
     map: Map,
-    player: Player
+    player: Player,
+    camera: Camera
 }
 
 impl State {
@@ -29,22 +33,27 @@ impl State {
             Self {
                 map : map_builder.map,
                 player: Player::new(map_builder.player_start),
+                camera: Camera::new(map_builder.player_start)
             }
     }
 }
 
 impl GameState for State {
+    
     fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.set_active_console(0);
         ctx.cls();
-        self.player.update(ctx, &self.map);
-        self.map.render(ctx);
-        self.player.render(ctx);
+        ctx.set_active_console(1);
+        ctx.cls();
+        self.player.update(ctx, &self.map, &mut self.camera);
+        self.map.render(ctx, &self.camera);
+        self.player.render(ctx, &self.camera);
     }
     
 }
 
 fn main() -> BError {
-    let context = BTermBuilder::simple80x50()
+    let context = BTermBuilder::new()
         .with_title("Dungeon Crawler")
         .with_fps_cap(30.0)
         .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
